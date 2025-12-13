a75ae757f5bced2b5bfe0734848ca51af7ba5d2c
Thirupathi <thirupathipegada2026@gmail.com>
2025-12-13 21:37:25 +0530

    web dev commit

---
+++ dom/script.js
@@ -0,0 +1,64 @@
+/*var h=document.querySelector('h1') 
+console.log(h)
+
+h.innerHTML='hello gud afternoon akshsunny' 
+h.style.color='green'
+h.style.backgroundColor=' black'
+
+h.addEventListener('click',function(){
+console.log("hey guys"); 
+}) 
+*/
+
+/*
+var h1=document.querySelector('h1')
+
+h1.addEventListener('click',function(){
+h1.innerHTML='hello miss world of kitss' 
+h1.style.color='green'
+h1.style.backgroundColor=' pink'
+console.log("hello mr sree") 
+})
+
+*/
+ 
+ // DAY 1 ADEVENTLISTENER PART-1 //
+
+/* var h=document.querySelector('h1') 
+console.log(h) */
+
+
+/* var h=document.querySelectorAll('h1') 
+console.log(h) 
+
+
+var h=document.querySelectorAll('h1') 
+console.log(h{0})
+
+var h=document.querySelectorAll('h1') 
+console.log(h{1})
+
+var h=document.querySelectorAll('h1') 
+console.log(h{2})
+
+
+
+var h=document.querySelectorAll('h1') 
+console.log(h[3])
+
+
+
+var h=document.querySelectorAll('#queen') 
+console.log(h)
+
+
+  ///////     GETELEMENTBYID   ///////
+
+  var h=document.getElementById('queen') 
+console.log(h)
+
+*/
+
+
+var h=document.querySelectorAll('box') 
+console.log(h)
