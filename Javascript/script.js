03be6432f5973354ddef81a353997a74c59cf9ae
Thirupathi <thirupathipegada2026@gmail.com>
2025-12-23 21:38:56 +0530

    changes in files

---
+++ Javascript/script.js
@@ -0,0 +1,47 @@
+console.log("Hello print my words and i hope you like it");
+
+console.log("You know , you are my best friend");
+
+console.warn(" you are driving in the wrong direction, pls go in a safe direction ");
+
+console.error( " It might you are entered input is incorrect");
+
+alert(" flm class will be start soon")
+
+
+//variables
+
+
+
+var a ;      //{ var a = declaration}
+a=10;           //HERE A IS INITIALIZATION
+console.log(a)    
+
+
+
+
+a="Hello my dear FRIENDS , I hope you are doing Well . I hope you are happy with ongoing sessions in our web development class"
+console.log(a)  
+
+
+var a ;
+a="Start by doing what's necessary; then do what's poosible; suddenly you are doing impossible; Nothing is impossible , the word itself says is possible"
+console.log(a)
+
+
+
+//types of arithmetic operators;
+
+var a =2;
+var b=3;
+console.log(a+b)     //addition +
+console.log(a-b)      // subtraction - 
+console.log(a*b)       //Multiplication
+console.log(a/b)        //division
+console.log(a%b)        //Modulo
+
+
+
+
+/////DAY2
+
