1f99e70700528721de5d2b86c8e0691b35e104cf
Thirupathi <thirupathipegada2026@gmail.com>
2026-01-04 14:05:47 +0530

    changes today

--- dom/script.js
+++ dom/script.js
@@ -1,4 +1,4 @@
-/*var h=document.querySelector('h1') 
+var h=document.querySelector('h1') 
 console.log(h)
 
 h.innerHTML='hello gud afternoon akshsunny' 
@@ -8,57 +8,44 @@ h.style.backgroundColor=' black'
 h.addEventListener('click',function(){
 console.log("hey guys"); 
 }) 
-*/
 
-/*
+
+
 var h1=document.querySelector('h1')
 
 h1.addEventListener('click',function(){
 h1.innerHTML='hello miss world of kitss' 
 h1.style.color='green'
 h1.style.backgroundColor=' pink'
-console.log("hello mr sree") 
+console.log("hello mr hero") 
 })
 
-*/
- 
- // DAY 1 ADEVENTLISTENER PART-1 //
 
-/* var h=document.querySelector('h1') 
-console.log(h) */
+var h=document.querySelector('h1') 
+console.log(h)
 
 
-/* var h=document.querySelectorAll('h1') 
+var h=document.querySelectorAll('h1') 
 console.log(h) 
 
 
-var h=document.querySelectorAll('h1') 
-console.log(h{0})
 
-var h=document.querySelectorAll('h1') 
-console.log(h{1})
 
-var h=document.querySelectorAll('h1') 
-console.log(h{2})
 
 
 
 var h=document.querySelectorAll('h1') 
+console.log(h[0])
+console.log(h[1])
+console.log(h[2])
 console.log(h[3])
 
+var h=document.querySelector('#hero')
+console.log(h)
 
-
-var h=document.querySelectorAll('#queen') 
+var h=document.getElementById('hero') 
 console.log(h)
 
 
-  ///////     GETELEMENTBYID   ///////
-
-  var h=document.getElementById('queen') 
-console.log(h)
-
-*/
 
 
-var h=document.querySelectorAll('box') 
-console.log(h)
---
+++ dom/script2.js
@@ -0,0 +1,8 @@
+/** @type {HTMLELEMENT}   */
+
+var box=document.querySelector("#box");
+var btn=document.querySelector(".btn");
+
+ btn.addEventListener("click", function() {
+    box.style.backgroundColor="red"
+ });
